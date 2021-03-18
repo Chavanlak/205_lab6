e6a60e96acff51e9a708aaa83c75448d4ea90b24
Chavanlak <chavanlak.p@ku.th>
2021-03-18 17:29:21 +0700

    a[b][c]='+';

--- 6220503198_drawing machine.c
+++ 6220503198_drawing machine.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+#include<string.h>
+{
+    int n,i,j;
+ char x[250];
+ scanf("%d",&n);
+ scanf("%s",y);
+ int a[n][n];
+ for(i=0;i<n;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+      a[i][j]='.';
+  }
+ }
+  int b=0,c=0;
+ for(i=0;i<strlen(y);i++)
+ {
+  if(x[i]=='U')
+  {
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+   if(b-1>=0)
+   b--;
+   if(a[b][c]=='.'||a[b][c]=='|')
+   a[b][c]='|';
+   else if(a[b][c]!='-'||a[b][c]!='+')
+   a[b][c]='+';
+  }
