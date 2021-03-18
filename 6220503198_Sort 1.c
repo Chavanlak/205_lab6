ffbd39b9f9194ae1d4c72cde2aada6fda8c8ce28
Chavanlak <chavanlak.p@ku.th>
2021-03-18 16:48:11 +0700

    char a[6][1000],temp[1000];

--- 6220503198_Sort 1.c
+++ 6220503198_Sort 1.c
@@ -3,18 +3,19 @@
 int main()
 {
     int n,i,j;
-    scanf("%d",&n);
-    char a[n][1000],temp[1000];
+    //scanf("%d",&n);
+    char a[6][1000],temp[1000];
 
-    for(i=0;i<n;i++)
+    for(i=0;i<6;i++)
     {
 
+        printf("Enter : ");
         scanf("%s",a[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<6;i++)
     {
 
-        for(j=i+1;j<n;j++)
+        for(j=i+1;j<6;j++)
         {
             if(strcmp(a[i],a[j])>0);
             {
@@ -26,7 +27,7 @@ int main()
         }
     }
     printf("Out put \n");
-    for(i=0;i<n;i++)
+    for(i=0;i<5;i++)
     {
         printf("%s\n",a[i]);
     }
