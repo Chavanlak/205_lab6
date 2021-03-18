68be03e1ec5df62670259f98bd02b66484eff4df
Chavanlak <chavanlak.p@ku.th>
2021-03-18 17:49:36 +0700

    printf("%d %d",a,b);

--- 6220503198_2.c
+++ 6220503198_2.c
@@ -4,6 +4,20 @@ int main()
     int a=0,b=0,i,j;
     scanf("%d",&j);
     char x[j],y[j];
-    scanf("%s",a);
-    scanf("%s",b);
+    scanf("%s",x);
+    scanf("%s",y);
+    for ( i = 0; i < j; i++)
+    {
+       if (y[i]==x[i])
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    printf("%d %d",a,b);
 }
+
+
